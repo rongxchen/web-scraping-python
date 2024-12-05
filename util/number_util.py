c49a3d2c8f14e80cb83f28cb9d33e73c04eb6a77
kennychen <chenrongxin20020102@gmail.com>
2024-12-05 15:30:11 +0800

    improve number util

--- util/number_util.py
+++ util/number_util.py
@@ -15,10 +15,10 @@ def to_number(number: Union[str, int, float], to_float: bool = False):
     num = -1
     if isinstance(number, int) or isinstance(number, float):
         num = number
-    if isinstance(number, str):
+    elif isinstance(number, str):
         if number.endswith("万"):
             num = int(float(number[:-1]) * 10000)
-        if number.endswith("亿"):
+        elif number.endswith("亿"):
             num = int(float(number[:-1]) * 100000000)
         if not is_number(num):
             raise ValueError("Invalid number string")
